5b75ac76df127a43b9eea59b3069695f06166795
Konrad3453 <konrad.baj98@gmail.com>
2024-11-28 04:43:16 +0100

    adding count chars

--- main.py
+++ main.py
@@ -4,8 +4,15 @@ def count(text):
     return len(count)
 
 
-
-
+def charCount(text):
+    alphabet = {}
+    for i in text:
+        to_lower = i.lower()
+        if to_lower in alphabet:
+            alphabet[to_lower] += 1
+        else:
+            alphabet[to_lower] =1
+    return alphabet
 
 
 def main():
@@ -14,4 +21,5 @@ def main():
     with open(path_to_file) as f:
         file_contents = f.read()
     print(count(file_contents))
+    print(charCount(file_contents))
 main()
